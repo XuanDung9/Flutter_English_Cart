42f1b4df6ca6cc7fe65f2bbe88a24ad6b6445cad
XuanDung09 <xdug94@gmail.com>
2025-03-28 10:09:44 +0700

    Decoration for home_page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -12,7 +12,14 @@ class HomePage extends StatefulWidget {
 
 class _MyWidgetState extends State<HomePage> {
   int _currentIndex = 0;
-  PageController _pageController = new PageController();
+  late PageController _pageController;
+
+  @override
+  void initState() {
+    _pageController = PageController(viewportFraction: 0.8);
+    // TODO: implement initState
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,14 +38,15 @@ class _MyWidgetState extends State<HomePage> {
 
       body: Container(
         width: double.infinity,
-        margin: const EdgeInsets.symmetric(horizontal: 24),
         child: Column(
           children: [
             Container(
               height: size.height * 1 / 10,
+              padding: const EdgeInsets.symmetric(horizontal: 24),
+              // margin: const EdgeInsets.symmetric(horizontal: 24),
               alignment: Alignment.centerLeft,
               child: Text(
-                '"It is a mazing how complete the delution that beauty is goodness. "',
+                '"It is a mazing how complete the delution that beauty is goodness . "',
                 style: AppStyles.h5.copyWith(
                   fontSize: 16,
                   color: AppColor.textColor,
@@ -56,74 +64,90 @@ class _MyWidgetState extends State<HomePage> {
                 },
                 itemCount: 5,
                 itemBuilder: (context, index) {
-                  return Container(
-                    padding: const EdgeInsets.all(16),
-                    decoration: BoxDecoration(
-                      color: AppColor.primaryColor,
-                      borderRadius: BorderRadius.all(Radius.circular(24)),
-                    ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Container(
-                          alignment: Alignment.centerRight,
-                          child: Image.asset(
-                            AppAssets.heart,
-                            width: 50,
-                            height: 50,
+                  return Padding(
+                    padding: const EdgeInsets.all(8),
+                    child: Container(
+                      padding: const EdgeInsets.all(16),
+                      decoration: BoxDecoration(
+                        color: AppColor.primaryColor,
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black26,
+                            offset: Offset(3, 6),
+                            blurRadius: 6,
                           ),
-                        ),
-                        RichText(
-                          maxLines: 1,
-                          overflow: TextOverflow.ellipsis,
-                          textAlign: TextAlign.start,
-                          text: TextSpan(
-                            text: 'B',
-                            style: TextStyle(
-                              fontFamily: FontFamily.sen,
-                              fontSize: 89,
-                              fontWeight: FontWeight.bold,
-                              shadows: [
-                                BoxShadow(
-                                  color: Colors.black38,
-                                  offset: Offset(3, 6),
+                        ],
+                        borderRadius: BorderRadius.all(Radius.circular(24)),
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            alignment: Alignment.centerRight,
+                            child: Image.asset(
+                              AppAssets.heart,
+                              width: 50,
+                              height: 50,
+                            ),
+                          ),
+                          RichText(
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                            textAlign: TextAlign.start,
+                            text: TextSpan(
+                              text: 'B',
+                              style: TextStyle(
+                                fontFamily: FontFamily.sen,
+                                fontSize: 89,
+                                fontWeight: FontWeight.bold,
+                                shadows: [
+                                  BoxShadow(
+                                    color: Colors.black38,
+                                    offset: Offset(3, 6),
+                                  ),
+                                ],
+                              ),
+                              children: [
+                                TextSpan(
+                                  text: 'eautiful',
+                                  style: TextStyle(
+                                    fontFamily: FontFamily.sen,
+                                    fontSize: 56,
+                                    fontWeight: FontWeight.bold,
+                                    shadows: [
+                                      BoxShadow(
+                                        color: Colors.black38,
+                                        offset: Offset(3, 6),
+                                      ),
+                                    ],
+                                  ),
                                 ),
                               ],
                             ),
-                            children: [
-                              TextSpan(
-                                text: 'eautiful',
-                                style: TextStyle(
-                                  fontFamily: FontFamily.sen,
-                                  fontSize: 56,
-                                  fontWeight: FontWeight.bold,
-                                  shadows: [
-                                    BoxShadow(
-                                      color: Colors.black38,
-                                      offset: Offset(3, 6),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ],
                           ),
-                        ),
 
-                        Padding(
-                          padding: const EdgeInsets.only(top: 24),
-                          child: Text(
-                            '"Think of all the beauty still left around you and be happy."',
-                            style: AppStyles.h4.copyWith(letterSpacing: 1),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 24),
+                            child: Text(
+                              '"Think of all the beauty still left around you and be happy."',
+                              style: AppStyles.h4.copyWith(
+                                letterSpacing: 1,
+                                color: AppColor.textColor,
+                              ),
+                            ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
                   );
                 },
               ),
             ),
+            // indicator
             Container(
               height: 12,
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              alignment: Alignment.center,
               margin: const EdgeInsets.symmetric(vertical: 12),
               child: ListView.builder(
                 scrollDirection: Axis.horizontal,
@@ -148,8 +172,8 @@ class _MyWidgetState extends State<HomePage> {
 
   Widget buildIndicator(bool isActive, Size size) {
     return Container(
-      height: 12,
-      margin: const EdgeInsets.symmetric(horizontal: 10),
+      height: 8,
+      margin: const EdgeInsets.symmetric(horizontal: 16),
       width: isActive ? size.width * 1 / 5 : 24,
       decoration: BoxDecoration(
         color: isActive ? AppColor.lightBlue : AppColor.lightGray,
